dc8fe2fed5ca6636807e2f532c18dbd51aef7cc7
leontech <martinleontech23@gmail.com>
2022-10-09 18:08:35 +0300

    --show doc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from gtts import gTTS
+from assets.leonResources import color,custom_output,user_input
 import os
 from PyPDF2 import PdfReader
 
@@ -17,7 +18,24 @@ def listFiles():
             if os.path.splitext(f)[1] in applicationStore.supportedDocuments:
                 applicationStore.AvailableFiles.append(f)
     
+    
+    # display files
+    documents=applicationStore.AvailableFiles
+    print(documents)
+    for num in range(0,len(documents),2):
+        print(num)
+        if num-2==-2:
+            pass
+        else:
+            currentDoc=documents[num-2]
+            nextNum=num-1
+            nextDoc=documents[nextNum]
+            custom_output.info(f"{num-2}{currentDoc} {nextNum}{nextDoc}",color.red)
+        
+        
+    
 listFiles()
+
     
 
 # temp = open('./SHE HACKS II.pdf', 'rb')

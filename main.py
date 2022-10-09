64470c3943bbe2fe3934ff367a6c4c6e5f102f68
leontech <martinleontech23@gmail.com>
2022-10-09 17:34:57 +0300

    -list-documents

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 from gtts import gTTS
 import os
 from PyPDF2 import PdfReader
-temp = open('./SHE HACKS II.pdf', 'rb')
 
-PDF_read = PdfReader(temp)
-first_page = PDF_read.getPage(0)
-text=first_page.extractText()
-language = 'en'
-myobj = gTTS(text=text, lang=language, slow=False)
-myobj.save("welcome.mp3")
-os.system("mpg321 welcome.mp3")
+class applicationStore:
+    DocumentName=""
+    NarratorSpeed=""
+    language='en' #default
+    supportedDocuments=['.pdf','.docx']
+    AvailableFiles=[]
+    
+def listFiles():
+    path=os.getcwd()
+    for (root, dirs, file) in os.walk(path):
+        for f in file:
+            if os.path.splitext(f)[1] in applicationStore.supportedDocuments:
+                applicationStore.AvailableFiles.append(f)
+    
+listFiles()
+    
+
+# temp = open('./SHE HACKS II.pdf', 'rb')
+# PDF_read = PdfReader(temp)
+# first_page = PDF_read.getPage(0)
+# text=first_page.extractText()
+# language = 'en'
+# myobj = gTTS(text=text, lang=language, slow=False)
+# myobj.save("welcome.mp3")
+# os.system("mpg321 welcome.mp3")
